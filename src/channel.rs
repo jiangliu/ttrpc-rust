4bd6465dda1d6cbb1b9cdf863e5e25911cfd09ab
Liu Jiang <gerry@linux.alibaba.com>
2020-04-24 11:46:28 +0800

    channel: prepare to support non-blocking IO
    Current channel/client/server implementation uses dedicated threads and
    blocking mode to do underlying socket IO. This designs may cost too much
    resources when supporting hign density scenarios. So refine the channel
    design to prepare for non-blocking IOs.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/channel.rs
+++ src/channel.rs
@@ -19,12 +19,25 @@ use std::os::unix::io::RawFd;
 use crate::error::{get_rpc_status, Error, Result};
 use crate::ttrpc::Code;
 
+/// ProtoBuf request message
+pub const MESSAGE_TYPE_REQUEST: u8 = 0x1;
+/// ProtoBuf reply message
+pub const MESSAGE_TYPE_RESPONSE: u8 = 0x2;
+
 const MESSAGE_HEADER_LENGTH: usize = 10;
 const MESSAGE_LENGTH_MAX: usize = 4 << 20;
 
-pub const MESSAGE_TYPE_REQUEST: u8 = 0x1;
-pub const MESSAGE_TYPE_RESPONSE: u8 = 0x2;
+const SOCK_DISCONNECTED: &str = "socket disconnected";
 
+fn sock_error_msg(closed: bool, msg: String) -> Error {
+    if closed {
+        return Error::Socket(SOCK_DISCONNECTED.to_string());
+    }
+
+    get_rpc_status(Code::INVALID_ARGUMENT, msg)
+}
+
+/// ProtoBuf message header.
 #[derive(Default, Debug)]
 pub struct MessageHeader {
     pub length: u32,
@@ -33,151 +46,249 @@ pub struct MessageHeader {
     pub flags: u8,
 }
 
-const SOCK_DISCONNECTED: &str = "socket disconnected";
+impl MessageHeader {
+    /// Decode a message header from a data buffer encoded by the ProtoBuf protocol.
+    pub fn decode(buf: &[u8]) -> Result<Self> {
+        if buf.len() != MESSAGE_HEADER_LENGTH {
+            return Err(sock_error_msg(
+                false,
+                format!("Message header length {} is incorrect", buf.len()),
+            ));
+        }
 
-fn sock_error_msg(size: usize, msg: String) -> Error {
-    if size == 0 {
-        return Error::Socket(SOCK_DISCONNECTED.to_string());
+        let mut mh = MessageHeader::default();
+        let mut covbuf: &[u8] = &buf[..4];
+        mh.length = covbuf.read_u32::<BigEndian>().map_err(err_to_RpcStatus!(
+            Code::INVALID_ARGUMENT,
+            e,
+            ""
+        ))?;
+        let mut covbuf: &[u8] = &buf[4..8];
+        mh.stream_id = covbuf.read_u32::<BigEndian>().map_err(err_to_RpcStatus!(
+            Code::INVALID_ARGUMENT,
+            e,
+            ""
+        ))?;
+        mh.type_ = buf[8];
+        mh.flags = buf[9];
+
+        if !mh.is_valid() {
+            return Err(get_rpc_status(
+                Code::INVALID_ARGUMENT,
+                format!(
+                    "message length {} exceed maximum message size of {}",
+                    mh.length, MESSAGE_LENGTH_MAX
+                ),
+            ));
+        }
+
+        Ok(mh)
     }
 
-    get_rpc_status(Code::INVALID_ARGUMENT, msg)
+    /// Encode a message header into a data buffer using the ProtoBuf protocol.
+    pub fn encode(&self) -> Vec<u8> {
+        let mut buf = vec![0u8; MESSAGE_HEADER_LENGTH];
+
+        let mut covbuf: &mut [u8] = &mut buf[..4];
+        BigEndian::write_u32(&mut covbuf, self.length);
+        let mut covbuf: &mut [u8] = &mut buf[4..8];
+        BigEndian::write_u32(&mut covbuf, self.stream_id);
+        buf[8] = self.type_;
+        buf[9] = self.flags;
+
+        buf
+    }
+
+    /// Check whether it's a request message.
+    pub fn is_request(&self) -> bool {
+        self.type_ == MESSAGE_TYPE_REQUEST
+    }
+
+    /// Check whether it's a response message.
+    pub fn is_response(&self) -> bool {
+        self.type_ == MESSAGE_TYPE_RESPONSE
+    }
+
+    /// Check whether the message header is valid or not.
+    pub fn is_valid(&self) -> bool {
+        self.length <= MESSAGE_LENGTH_MAX as u32 && (self.is_request() || self.is_response())
+    }
+}
+
+/// A buffer to receive data from underlying socket.
+pub struct ReadBuffer {
+    buf: Vec<u8>,
+    len: usize,
+    pos: usize,
+}
+
+impl ReadBuffer {
+    /// Create a ReadBuffer with capacity to receive `count` bytes.
+    pub fn new(count: usize) -> Self {
+        let mut buf: Vec<u8> = Vec::with_capacity(count);
+        // It's safe because the vector is used as a buffer to receive message.
+        unsafe {
+            buf.set_len(count);
+        }
+
+        ReadBuffer {
+            buf,
+            len: count,
+            pos: 0,
+        }
+    }
+
+    /// Receive data from the socket `fd`.
+    ///
+    /// Returns:
+    /// - Ok(n) when n > 0: bytes of data received.
+    /// - Ok(0): socket has been closed.
+    /// - Err(e): failed to receive data from the socket.
+    pub fn recv(&mut self, fd: RawFd) -> Result<usize> {
+        loop {
+            match recv(fd, &mut self.buf[self.pos..], MsgFlags::empty()) {
+                Ok(l) => {
+                    self.pos += l;
+                    return Ok(l);
+                }
+
+                Err(e) => {
+                    if e != ::nix::Error::from_errno(::nix::errno::Errno::EINTR) {
+                        return Err(Error::Socket(e.to_string()));
+                    }
+                }
+            }
+        }
+    }
+
+    /// Check whether all data has been received.
+    pub fn is_ready(&self) -> bool {
+        self.len == self.pos
+    }
+}
+
+impl Into<Vec<u8>> for ReadBuffer {
+    fn into(self) -> Vec<u8> {
+        self.buf
+    }
+}
+
+impl From<Vec<u8>> for ReadBuffer {
+    fn from(buf: Vec<u8>) -> Self {
+        let len = buf.len();
+
+        ReadBuffer { buf, len, pos: 0 }
+    }
 }
 
 fn read_count(fd: RawFd, count: usize) -> Result<Vec<u8>> {
-    let mut len = 0;
-    let mut v: Vec<u8> = Vec::with_capacity(count);
-    // It's safe because the vector is used as a buffer to receive message.
-    unsafe { v.set_len(count); }
+    let mut buf = ReadBuffer::new(count);
 
-    loop {
-        match recv(fd, &mut v[len..], MsgFlags::empty()) {
-            Ok(l) => {
-                len += l;
-                // when socket peer closed, it would return 0.
-                if len == count || l == 0 {
-                    break;
-                }
+    while !buf.is_ready() {
+        match buf.recv(fd)? {
+            0 => {
+                return Err(sock_error_msg(
+                    true,
+                    format!(
+                        "Socket get closed after receiving {} out of {} bytes data",
+                        buf.pos, buf.len
+                    ),
+                ))
             }
-
-            Err(e) => {
-                if e != ::nix::Error::from_errno(::nix::errno::Errno::EINTR) {
-                    return Err(Error::Socket(e.to_string()));
-                }
-            }
+            _ => continue,
         }
     }
 
-    Ok(v[0..len].to_vec())
+    Ok(buf.into())
 }
 
-fn write_count(fd: RawFd, buf: &[u8], count: usize) -> Result<usize> {
-    let mut len = 0;
-
-    loop {
-        match send(fd, &buf[len..], MsgFlags::empty()) {
-            Ok(l) => {
-                len += l;
-                if len == count {
-                    break;
-                }
-            }
-
-            Err(e) => {
-                if e != ::nix::Error::from_errno(::nix::errno::Errno::EINTR) {
-                    return Err(Error::Socket(e.to_string()));
-                }
-            }
-        }
-    }
-
-    Ok(len)
-}
-
-fn read_message_header(fd: RawFd) -> Result<MessageHeader> {
-    let buf = read_count(fd, MESSAGE_HEADER_LENGTH)?;
-    let size = buf.len();
-    if size != MESSAGE_HEADER_LENGTH {
-        return Err(sock_error_msg(
-            size,
-            format!("Message header length {} is too small", size),
-        ));
-    }
-
-    let mut mh = MessageHeader::default();
-    let mut covbuf: &[u8] = &buf[..4];
-    mh.length =
-        covbuf
-            .read_u32::<BigEndian>()
-            .map_err(err_to_RpcStatus!(Code::INVALID_ARGUMENT, e, ""))?;
-    let mut covbuf: &[u8] = &buf[4..8];
-    mh.stream_id =
-        covbuf
-            .read_u32::<BigEndian>()
-            .map_err(err_to_RpcStatus!(Code::INVALID_ARGUMENT, e, ""))?;
-    mh.type_ = buf[8];
-    mh.flags = buf[9];
-
-    Ok(mh)
-}
-
+/// Receive a fully message from the underlying socket 'fd' in blocking mode.
 pub fn read_message(fd: RawFd) -> Result<(MessageHeader, Vec<u8>)> {
-    let mh = read_message_header(fd)?;
+    let buf = read_count(fd, MESSAGE_HEADER_LENGTH)?;
+    let mh = MessageHeader::decode(&buf)?;
     trace!("Got Message header {:?}", mh);
 
-    if mh.length > MESSAGE_LENGTH_MAX as u32 {
-        return Err(get_rpc_status(
-            Code::INVALID_ARGUMENT,
-            format!(
-                "message length {} exceed maximum message size of {}",
-                mh.length, MESSAGE_LENGTH_MAX
-            ),
-        ));
-    }
-
     let buf = read_count(fd, mh.length as usize)?;
-    let size = buf.len();
-    if size != mh.length as usize {
-        return Err(sock_error_msg(
-            size,
-            format!("Message length {} is not {}", size, mh.length),
-        ));
-    }
     trace!("Got Message body {:?}", buf);
 
     Ok((mh, buf))
 }
 
-fn write_message_header(fd: RawFd, mh: MessageHeader) -> Result<()> {
-    let mut buf = [0u8; MESSAGE_HEADER_LENGTH];
+/// A buffer to send data from underlying socket.
+pub struct WriteBuffer {
+    buf: Vec<u8>,
+    len: usize,
+    pos: usize,
+}
 
-    let mut covbuf: &mut [u8] = &mut buf[..4];
-    BigEndian::write_u32(&mut covbuf, mh.length);
-    let mut covbuf: &mut [u8] = &mut buf[4..8];
-    BigEndian::write_u32(&mut covbuf, mh.stream_id);
-    buf[8] = mh.type_;
-    buf[9] = mh.flags;
+impl WriteBuffer {
+    /// Send data to the socket `fd`.
+    ///
+    /// Returns:
+    /// - Ok(n) when n > 0: bytes of data sent.
+    /// - Ok(0): socket has been closed.
+    /// - Err(e): failed to send data from the socket.
+    pub fn send(&mut self, fd: RawFd) -> Result<usize> {
+        loop {
+            match send(fd, &self.buf[self.pos..], MsgFlags::empty()) {
+                Ok(l) => {
+                    self.pos += l;
+                    return Ok(l);
+                }
 
-    let size = write_count(fd, &buf, MESSAGE_HEADER_LENGTH)?;
-    if size != MESSAGE_HEADER_LENGTH {
-        return Err(sock_error_msg(
-            size,
-            format!("Send Message header length size {} is not right", size),
-        ));
+                Err(e) => {
+                    if e != ::nix::Error::from_errno(::nix::errno::Errno::EINTR) {
+                        return Err(Error::Socket(e.to_string()));
+                    }
+                }
+            }
+        }
+    }
+
+    /// Check whether all data has been sent out.
+    pub fn is_done(&self) -> bool {
+        self.pos == self.len
+    }
+}
+
+impl Into<Vec<u8>> for WriteBuffer {
+    fn into(self) -> Vec<u8> {
+        self.buf
+    }
+}
+
+impl From<Vec<u8>> for WriteBuffer {
+    fn from(buf: Vec<u8>) -> Self {
+        let len = buf.len();
+
+        WriteBuffer { buf, len, pos: 0 }
+    }
+}
+
+fn write_count(fd: RawFd, buf: &mut WriteBuffer) -> Result<()> {
+    while !buf.is_done() {
+        match buf.send(fd)? {
+            0 => {
+                return Err(sock_error_msg(
+                    true,
+                    format!(
+                        "Socket gets closed after sending {} out of {} bytes data",
+                        buf.pos, buf.len
+                    ),
+                ))
+            }
+            _ => continue,
+        }
     }
 
     Ok(())
 }
 
+/// Send a fully message to the underlying socket 'fd' in blocking mode.
 pub fn write_message(fd: RawFd, mh: MessageHeader, buf: Vec<u8>) -> Result<()> {
-    write_message_header(fd, mh)?;
+    let mut hbuf = WriteBuffer::from(mh.encode());
+    write_count(fd, &mut hbuf)?;
 
-    let size = write_count(fd, &buf, buf.len())?;
-    if size != buf.len() {
-        return Err(sock_error_msg(
-            size,
-            format!("Send Message length size {} is not right", size),
-        ));
-    }
-
-    Ok(())
+    let mut wbuf = WriteBuffer::from(buf);
+    write_count(fd, &mut wbuf)
 }
